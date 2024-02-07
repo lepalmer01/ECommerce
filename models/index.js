0dd1b7077e178e9b29d4baaeb4477033e4ce25e1
LE Palmer <lpalmer@ucsd.edu>
2024-02-07 12:16:55 -0800

    created columns for each of the 4 models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
 // Categories have many Products
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsTo(Category, { foreignKey: 'category_id' });
+Category.hasMany(Product, { foreignKey: 'category_id' });
 
+// Products belongToMany Tags (through ProductTag)
 // Tags belongToMany Products (through ProductTag)
+
+Product.belongsToMany(Tag, { through: ProductTag, foreignKey: 'product_id' });
+Tag.belongsToMany(Product, { through: ProductTag, foreignKey: 'tag_id' });
 
 module.exports = {
   Product,
